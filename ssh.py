463d9bce5f9a16c369223796acafc73e4de672d8
Sliverkiss <1393579810@qq.com>
2024-06-14 13:37:53 +0800

    Update ssh.py

--- ssh.py
+++ ssh.py
@@ -1,6 +1,7 @@
 import os
 import json
 import paramiko
+import requests
 
 # 从环境变量中读取 ACCOUNTS_JSON
 accounts_json = os.getenv('ACCOUNTS')
@@ -14,9 +15,17 @@ def ssh_connect(host, username, password):
         transport.connect(username=username, password=password)
         ssh_status = "SSH连接成功"
         print(f"SSH连接成功。")
+
+        if bark:
+            url = f"https://api.day.app/{bark}/Serv00自动登录/账号 {username} SSH连接成功。"
+            response = requests.get(url)
     except Exception as e:
         ssh_status = f"SSH连接失败，错误信息: {e}"
         print(f"SSH连接失败: {e}")
+
+        if bark:
+            url = f"https://api.day.app/{bark}/Serv00自动登录/账号 {username} SSH连接失败，请检查账号和密码是否正确。"
+            response = requests.get(url)
     finally:
         if transport is not None:
             transport.close()
