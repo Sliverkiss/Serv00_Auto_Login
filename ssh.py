2e88bb266ba1a8d1115e44b99580856c3f26f26e
qq35 <67598411+qq35@users.noreply.github.com>
2024-07-01 13:05:55 +0800

    Update ssh.py
    1

--- ssh.py
+++ ssh.py
@@ -7,29 +7,43 @@ import requests
 accounts_json = os.getenv('ACCOUNTS')
 accounts = json.loads(accounts_json)
 
-# 尝试通过SSH连接的函数
-def ssh_connect(host, username, password, bark):
-    transport = None
+# 尝试通过SSH连接的函数，并执行指定命令
+def ssh_connect(host, username, password, bark, command):
+    client = None
     try:
-        transport = paramiko.Transport((host, 22))
-        transport.connect(username=username, password=password)
-        ssh_status = "SSH连接成功"
-        print(f"SSH连接成功。")
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect(host, username=username, password=password)
+        stdin, stdout, stderr = client.exec_command(command)
 
-        if bark:
-            url = f"https://api.day.app/{bark}/Serv00自动登录/账号 {username} SSH连接成功。"
-            response = requests.get(url)
+        # 获取命令执行的输出
+        output = stdout.read().decode('utf-8')
+        error = stderr.read().decode('utf-8')
+
+        if error:
+            ssh_status = f"命令执行失败，错误信息: {error}"
+            print(ssh_status)
+            if bark:
+                url = f"https://api.day.app/{bark}/Serv00自动登录/账号 {username} 命令执行失败，错误信息: {error}"
+                response = requests.get(url)
+        else:
+            ssh_status = f"命令执行成功，输出: {output}"
+            print(ssh_status)
+            if bark:
+                url = f"https://api.day.app/{bark}/Serv00自动登录/账号 {username} 命令执行成功。"
+                response = requests.get(url)
+
     except Exception as e:
         ssh_status = f"SSH连接失败，错误信息: {e}"
-        print(f"SSH连接失败: {e}")
+        print(ssh_status)
 
         if bark:
             url = f"https://api.day.app/{bark}/Serv00自动登录/账号 {username} SSH连接失败，请检查账号和密码是否正确。"
             response = requests.get(url)
     finally:
-        if transport is not None:
-            transport.close()
+        if client is not None:
+            client.close()
 
 # 循环执行任务
 for account in accounts:
-    ssh_connect(account['host'], account['username'], account['password'], account["bark"])
+    ssh_connect(account['host'], account['username'], account['password'], account["bark"], '/home/cxzjswa/.npm-global/bin/pm2 resurrect')
